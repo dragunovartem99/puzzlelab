8933fa1261851afd28ba7c181aaceb4a62725459
Artem Dragunov <dragunovartem99@gmail.com>
2025-01-19 21:08:06 +0300

    Add first puzzle (hopefully)

---
+++ index.js
@@ -0,0 +1,10 @@
+// TODO: #1 Implement main API
+//  Client picks .csv dataset (lichess) and desired jobs
+//  Application serves the client with "lab" (analysis):
+//  - Statistics (bound to rating)
+//  - List of puzzles found
+//  This lab can be rendered on a frontend (digarams / list)
+// P.S. Client can export lab in .csv or .json
+function createLab(dataset, jobs) {
+
+}
