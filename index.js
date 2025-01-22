619b59b66633b9e4fc01b0ab48dd550d68a7dd9b
Artem Dragunov <dragunovartem99@gmail.com>
2025-01-22 15:19:29 +0300

    Refine main idea (comments)

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // TODO: #1 Implement main API
-//  Client picks .csv dataset (lichess) and desired jobs
-//  Application serves the client with "lab" (analysis):
-//  - Statistics (bound to rating)
-//  - List of puzzles found
-//  This lab can be rendered on a frontend (digarams / list)
-// P.S. Client can export lab in .csv or .json
-function createLab(dataset, jobs) {
+// Client picks .csv dataset (lichess) and desired job
+// Application serves the client with "lab" (analysis):
+// - Statistics (bound to rating)
+// - List of puzzles found
+// This lab can be rendered on a frontend (digarams / list)
+function createLab(dataset, job) {
+	// As example
+	job = {
+		name: "",
+		options: {},
+	}
+}
 
+function exportLab() {
+	// P.S. Client can export lab in .csv or .json
 }
