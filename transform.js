69b3076d32e20a51a73976497bacf98461085440
Artem Dragunov <dragunovartem99@gmail.com>
2025-01-25 22:50:32 +0300

    Restore the function

--- transform.js
+++ transform.js
@@ -4,5 +4,13 @@ import { mainDatasetHead } from "./datasets.js";
 import { Flow } from "./objects/Flow.js";
 import { Stage } from "./objects/Stage.js";
 
+let count = 0;
+
+function performance(puzzles) {
+	count += puzzles.length;
+	return puzzles;
+}
+
 await new Flow(mainDatasetHead, createWriteStream("./hey.csv"), new Stage(performance)).run();
 
+console.log(count);
