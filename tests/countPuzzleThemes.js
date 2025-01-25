12953e4f200671cad8985ddfd7ce35ab6692d913
Artem Dragunov <dragunovartem99@gmail.com>
2025-01-25 23:30:57 +0300

    Add more tests

---
+++ tests/countPuzzleThemes.js
@@ -0,0 +1,19 @@
+import { Flow } from "../objects/Flow.js";
+import { Stage } from "../objects/Stage.js";
+import { Requirements } from "../objects/Requirements.js";
+
+export default async function (dataset, payload) {
+	let count = 0;
+
+	const filter = new Requirements(payload).getFilter();
+
+	function performance(puzzles) {
+		return puzzles.filter((puzzle) => {
+			filter.check(puzzle) && count++;
+		});
+	}
+
+	await new Flow(dataset, new Stage(performance)).run();
+
+	return count;
+}
