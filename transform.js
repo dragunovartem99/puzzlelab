36d311713bfc0002a26a99fe5f0cfd5b33318672
Artem Dragunov <dragunovartem99@gmail.com>
2025-01-25 21:44:26 +0300

    Create transform draft

---
+++ transform.js
@@ -0,0 +1,37 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { Transform } from "node:stream";
+
+export class PuzzleReader extends Transform {
+	#onPuzzle;
+	#corruptedPuzzle = null;
+
+	constructor(onPuzzle) {
+		super({ objectMode: true });
+		this.#onPuzzle = onPuzzle;
+	}
+
+	_transform(chunk, _, callback) {
+		const puzzles = chunk.split("\n");
+
+		if (this.#corruptedPuzzle) {
+			console.log(this.#corruptedPuzzle);
+			puzzles[0] = this.#corruptedPuzzle + puzzles[0];
+			this.#corruptedPuzzle = 0;
+		}
+
+		this.#corruptedPuzzle = puzzles.pop();
+
+		if (this.#onPuzzle) {
+			puzzles.forEach(this.#onPuzzle);
+		}
+
+		this.push(puzzles.join("\n") + "\n");
+		callback();
+	}
+}
+
+const myTransform = new PuzzleReader();
+
+createReadStream("./samples/head-999.csv", { encoding: "ascii" })
+	.pipe(myTransform)
+	.pipe(createWriteStream("./hey.csv"))
